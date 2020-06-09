a7884aa49ab394e5f39f9a2f91a679c7eee213da
darshnablah <dwright758@gmail.com>
2020-06-09 13:15:39 -0500

    added event listener

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEL = document.querySelector("#tasks-to-do");
+var createTaskHandler = function() {
+    var listItemEL = document.createElement("li");
+    listItemEL.className = "task-item";
+    listItemEL.textContent = "This is a new task.";
+    tasksToDoEL.appendChild(listItemEL);
+    console.log(listItemEL);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
